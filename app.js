a04924954ad0cb2cc5d299dd565939df184bb082
清水紀行 <noriyuki.shimizu0612@gmail.com>
2019-02-18 23:37:24 +0900

    メイン処理の修正

--- app.js
+++ app.js
@@ -4,14 +4,18 @@ exports.__esModule = true;
 var ipinfoImpl_1 = require("./src/location/ipinfo/ipinfoImpl");
 var tokoroImpl_1 = require("./src/location/tokoro/tokoroImpl");
 var openWeatherMap_1 = require("./src/weather/openWeatherMap");
-var locationList = [];
+var locationList = new Array();
 locationList.push(new ipinfoImpl_1.IpinfoImpl());
 locationList.push(new tokoroImpl_1.TokoroImpl());
+var displayList = new Array();
+displayList.push({
+    text: '🌞'
+});
 locationList.forEach(function (location) {
     location.getLocation(function (latlon) {
         var lat = latlon[0];
         var lon = latlon[1];
         var openWeatherMap = new openWeatherMap_1.OpenWeatherMap(lat, lon);
-        openWeatherMap.getWeather();
+        openWeatherMap.getWeather(displayList);
     });
 });
