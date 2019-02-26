c2fa8ff3a418d18b784bbac8224aa230af81d64e
清水紀行 <noriyuki.shimizu0612@gmail.com>
2019-02-26 19:35:11 +0900

    メイン処理の修正

--- app.ts
+++ app.ts
@@ -4,25 +4,26 @@ import { Location } from './src/location/location';
 import { IpinfoImpl } from './src/location/ipinfo/ipinfoImpl';
 import { TokoroImpl } from './src/location/tokoro/tokoroImpl';
 import { OpenWeatherMap } from './src/weather/openWeatherMap';
+import { Convert } from './src/convert/convert';
+import { OpenWeatherMapConvert } from './src/convert/openWeatherMap/openWeatherMapConvert';
+import { OpenWeatherMapBitBar } from './src/bitbar/openWeatherMap/openWeatherMapBitBar';
 
 var locationList: Location[] = new Array();
 
 locationList.push(new IpinfoImpl());
 locationList.push(new TokoroImpl());
 
-var displayList: any[] = new Array();
-displayList.push({
-    text: '🌞'
-});
-
 locationList.forEach(location => {
-    location.getLocation(latlon => {
-        const lat = latlon[0];
-        const lon = latlon[1];
+    location.getLocation((latlon, address) => {
+        const openWeatherMap: OpenWeatherMap = new OpenWeatherMap(latlon, address);
 
-        const openWeatherMap: OpenWeatherMap = new OpenWeatherMap(lat, lon);
+        openWeatherMap.getWeather(weatherDataList => {
+            const openWeatherMapConvert: Convert = new OpenWeatherMapConvert(weatherDataList);
+            const convertWeatherData: {text: string; color: string; submenu: []}[] = openWeatherMapConvert.convert();
 
-        openWeatherMap.getWeather(displayList);
+            const openweatherMapBitBar: OpenWeatherMapBitBar = new OpenWeatherMapBitBar(convertWeatherData, address);
+            openweatherMapBitBar.display();
+        });
     });
 });
 
