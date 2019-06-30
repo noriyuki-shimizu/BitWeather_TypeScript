796fe7e53ebd0e7c9933bc1611a217865ed87009
清水紀行 <noriyuki.shimizu0612@gmail.com>
2019-06-30 14:56:55 +0900

    フォルダ階層見直しによるインポートの修正

--- src/convert/openWeatherMap/openWeatherMapConvert.ts
+++ src/convert/openWeatherMap/openWeatherMapConvert.ts
@@ -2,7 +2,7 @@ import { Convert } from '../convert';
 import { TimeDiff } from '../../date/timeDiff/timeDiff';
 import { Greenwich } from '../../date/timeDiff/greenwith/greenwich';
 import { Format } from '../../date/format/format';
-import { AggregateWeatherData } from './formatWeather/aggregate/aggregateWeatherData';
+import { AggregateWeatherData } from './formatWeather/aggregateWeatherData';
 
 /**
  * APIから取得されたデータを変換するクラス。
